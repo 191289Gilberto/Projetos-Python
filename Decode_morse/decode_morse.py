e4802e9ed616ba734b20d6b24e89ed6fd4cffe00
191289Gilberto <gilberto.ferreira@aluno.faculdadeimpacta.com.br>
2024-09-01 21:15:48 -0300

    novo

--- Decode_morse/decode_morse.py
+++ Decode_morse/decode_morse.py
@@ -14,14 +14,22 @@ from config import file_path, dict_morse
 
 def decode_morse(msg):
     '''
-    input : mensagem em código morse com as letras separadas por espaços
+    input : mensagem em código morse com letras separadas por um espaço e palavras separadas por dois espaços
     output : palavra escrito em letras e algarismos
     '''
-    msg_lst = msg.split(" ")
-    msg_claro = []
-    for letter in msg_lst:
-        msg_claro.append(dict_morse[letter])
-    return "".join(msg_claro)
+    # Divide a mensagem em palavras usando dois espaços como separador
+    words = msg.split('  ')  # Dois espaços indicam separação de palavras
+    decoded_message = []
+
+    for word in words:
+        if word:  # Verifica se a palavra não está vazia
+            letters = word.split(' ')  # Um espaço indica separação de letras
+            decoded_word = ''.join([dict_morse.get(letter, '?') for letter in letters])
+            decoded_message.append(decoded_word)
+        else:
+            decoded_message.append(' ')  # Adiciona um espaço para manter a separação entre palavras
+    
+    return ' '.join(decoded_message)
 
 def save_clear_msg_csv_hdr(msg_claro):
     '''
@@ -31,10 +39,12 @@ def save_clear_msg_csv_hdr(msg_claro):
     now = datetime.datetime.now()
     df = pd.DataFrame([[msg_claro, now]], columns=["mensagem", "datetime"])
     hdr = not os.path.exists(file_path)
-    df.to_csv(file_path, mode ="a", index = False, header=hdr)
+    df.to_csv(file_path, mode="a", index=False, header=hdr)
 
 if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python script.py '<morse_code_message>'")
+        sys.exit(1)
+    
     msg_claro = decode_morse(sys.argv[1])
     save_clear_msg_csv_hdr(msg_claro)
-    #print(save_clear_msg_csv_hdr.__doc__)
-    #print(pd.to_pickle.__doc__)
